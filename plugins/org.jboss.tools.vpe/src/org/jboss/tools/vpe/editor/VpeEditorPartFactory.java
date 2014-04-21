0818f98669fe8e389c673f9ea0df0a363acc25d0
Konstantin Marmalyukov <kmarmaliykov@exadel.com>
2014-04-21 19:25:51 +0300

    JBIDE-16925 Switch preview engine for new implementation of visual editor
    implemented choosing of correct visual part

--- plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/VpeEditorPartFactory.java
+++ plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/VpeEditorPartFactory.java
@@ -11,21 +11,39 @@
 package org.jboss.tools.vpe.editor;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.browser.BrowserInitializer;
 import org.eclipse.ui.part.EditorPart;
 import org.eclipse.wst.sse.ui.StructuredTextEditor;
 import org.jboss.tools.jst.web.ui.internal.editor.bundle.BundleMap;
 import org.jboss.tools.jst.web.ui.internal.editor.editor.IVisualEditor;
 import org.jboss.tools.jst.web.ui.internal.editor.editor.IVisualEditorFactory;
+import org.jboss.tools.vpe.xulrunner.browser.XulRunnerBrowser;
 
 public class VpeEditorPartFactory implements IVisualEditorFactory {
-
+	public static final String LOAD_XULRUNNER_ENGINE = "org.jboss.tools.vpe.engine.xulrunner"; //$NON-NLS-1$
+	
 	public IVisualEditor createVisualEditor(final EditorPart multiPageEditor, StructuredTextEditor textEditor, int visualMode, BundleMap bundleMap) {
-//		return new VpvEditor2(multiPageEditor, textEditor, visualMode, bundleMap);
+		boolean isHtmlFile = "html".equals(((org.eclipse.ui.IFileEditorInput)multiPageEditor.getEditorInput()).getFile().getFileExtension()); //$NON-NLS-1$
+		if (isHtmlFile) {
+			return getPreviewEditor(multiPageEditor, textEditor, visualMode, bundleMap);
+		} else {
+			if (XulRunnerBrowser.isCurrentPlatformOfficiallySupported() && !BrowserInitializer.isGTK3()) {
+				return getVpeEditor(multiPageEditor, textEditor, visualMode, bundleMap);
+			} else {
+				return getPreviewEditor(multiPageEditor, textEditor, visualMode, bundleMap);				
+			}
+		}
+	}
+	
+	private IVisualEditor getVpeEditor(final EditorPart multiPageEditor, StructuredTextEditor textEditor, int visualMode, BundleMap bundleMap) {
 		return new VpeEditorPart(multiPageEditor, textEditor, visualMode, bundleMap) {
-			public void doSave(IProgressMonitor monitor){
+			public void doSave(IProgressMonitor monitor) {
 				multiPageEditor.doSave(monitor);
 			}
 		};
 	}
 
+	private IVisualEditor getPreviewEditor(final EditorPart multiPageEditor, StructuredTextEditor textEditor, int visualMode, BundleMap bundleMap) {
+		return new VpvEditor2(multiPageEditor, textEditor, visualMode, bundleMap);
+	}
 }
