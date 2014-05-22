7f53ce42e181df408183c0e9ba03df557cc9e615
Konstantin Marmalyukov <kmarmaliykov@exadel.com>
2014-05-22 18:03:06 +0300

    Update GTK3 detection for vpe/vpv

--- plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/util/VpePlatformUtil.java
+++ plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/util/VpePlatformUtil.java
@@ -1,5 +1,7 @@
 package org.jboss.tools.vpe.editor.util;
 
+import java.lang.reflect.Field;
+
 import org.eclipse.core.runtime.Platform;
 
 public class VpePlatformUtil {
@@ -8,13 +10,32 @@ public class VpePlatformUtil {
 	
 	public static boolean isGTK3() {
 		if (Platform.WS_GTK.equals(Platform.getWS())) {
-			String gtk3 = System.getProperty(SWT_GTK3);
-			if (gtk3 == null) {
-				gtk3 = System.getenv(SWT_GTK3);
+			try {
+				Class<?> clazz = Class.forName("org.eclipse.swt.internal.gtk.OS"); //$NON-NLS-1$
+				Field field = clazz.getDeclaredField("GTK3"); //$NON-NLS-1$
+				boolean gtk3 = field.getBoolean(field);
+				return gtk3;
+			} catch (ClassNotFoundException e) {
+				return isGTK3Env();
+			} catch (NoSuchFieldException e) {
+				return false;
+			} catch (SecurityException e) {
+				return isGTK3Env();
+			} catch (IllegalArgumentException e) {
+				return isGTK3Env();
+			} catch (IllegalAccessException e) {
+				return isGTK3Env();
 			}
-			return !"0".equals(gtk3); //$NON-NLS-1$
 		}
 		return false;
+	}
+
+	private static boolean isGTK3Env() {
+		String gtk3 = System.getProperty(SWT_GTK3);
+		if (gtk3 == null) {
+			gtk3 = System.getenv(SWT_GTK3);
+		}
+		return !"0".equals(gtk3); //$NON-NLS-1$
 	}
 	
 	public static boolean isXulrunnerEnabled() {
