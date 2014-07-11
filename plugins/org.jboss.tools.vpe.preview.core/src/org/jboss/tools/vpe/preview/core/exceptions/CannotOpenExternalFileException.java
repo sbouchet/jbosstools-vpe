9fa8621954c05886747742478a017ead9e49c6dc
Konstantin Marmalyukov <kmarmaliykov@exadel.com>
2014-07-11 15:14:18 +0300

    Fix Jenkins build failure

--- plugins/org.jboss.tools.vpe.preview.core/src/org/jboss/tools/vpe/preview/core/exceptions/CannotOpenExternalFileException.java
+++ plugins/org.jboss.tools.vpe.preview.core/src/org/jboss/tools/vpe/preview/core/exceptions/CannotOpenExternalFileException.java
@@ -4,6 +4,14 @@ public class CannotOpenExternalFileException extends Exception {
 	private static final long serialVersionUID = -2090569707031789823L;
 	
 	public CannotOpenExternalFileException(String message) {
-		super(message, null, true, false);
+		super(message, null);
+	}
+	
+	@Override
+	/**
+	 * Disable showing stacktrace for this exception.
+	 */
+	public synchronized Throwable fillInStackTrace() {
+		return null;
 	}
 }
